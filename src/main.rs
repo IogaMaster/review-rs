9bd20ecbd8c32486b439d53d38b9e1e77f904cd5
IogaMaster <iogamastercode@gmail.com>
2024-01-19 16:10:32 -0700

    more stuff done

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::{collections::HashMap, env, error::Error, process::exit, borrow::Borrow};
+
+use octocrab::models::pulls::FileDiff;
+
+#[tokio::main]
+async fn main() {
     let args: Vec<String> = env::args().collect();
 
     let mut pr_num: String = String::new();
 
     if args.len() > 1 {
-        if args[1].contains("github.com/nixos/nixpkgs") == true {
+        if args[1].contains("github.com") == true {
             pr_num = args[1].split("/").last().unwrap().to_string();
         } else if args[1].to_string().parse::<i32>().is_ok() {
             pr_num = args[1].to_string();
@@ -15,5 +19,19 @@ fn main() {
         exit(1);
     }
 
-    let mut pr_url: String = format!("https://api.github.com/repos/nixos/nixpkgs/pulls/{pr_num}");
+    let mut changed_packages: Vec<String> = vec![];
+
+    for diff in octocrab::instance()
+        .pulls("nixos", "nixpkgs")
+        .list_files(pr_num.parse::<u64>().unwrap())
+        .await.unwrap().items
+    {
+        // Only get packages, not top-level or other changed files
+        if diff.filename.contains("pkgs") && !diff.filename.contains("top-level") {
+            // let mut package_name = diff.filename.rsplit("/").nth(1).unwrap().to_string();
+            changed_packages.push(diff.filename);
+        }
+    };
+    
+    println!("{:#?}", changed_packages) 
 }
